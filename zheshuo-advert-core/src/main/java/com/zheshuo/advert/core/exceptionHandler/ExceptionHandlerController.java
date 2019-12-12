8aeb1fee90b15943aff521c2fb844857093810a8
zhanglong <718168633@qq.com>
2019-12-12 13:47:19 +0800

    controller自定义异常响应处理

--- zheshuo-advert-core/src/main/java/com/zheshuo/advert/core/exceptionHandler/ExceptionHandlerController.java
+++ zheshuo-advert-core/src/main/java/com/zheshuo/advert/core/exceptionHandler/ExceptionHandlerController.java
@@ -1,14 +1,27 @@
 package com.zheshuo.advert.core.exceptionHandler;
 
+import com.zheshuo.advert.core.common.OutputDTO;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * created by zhanglong and since  2019/12/12  10:57 上午
- *
  * @description: 描述
  */
-@ControllerAdvice
+@ControllerAdvice("com.zheshuo.advert.controller")
 public class ExceptionHandlerController {
-
+    /**
+     * description 自定义拦截
+     */
+    @ResponseBody
+    @ExceptionHandler({Exception.class}) //指定拦截异常的类型
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) //自定义浏览器返回状态码
+    public OutputDTO<?> customExceptionHandler( Exception e ) {
+        return new OutputDTO<>().fail(e.getMessage());
+    }
 
 }
